beaf8301e3ccd5d7b325b9056ba3b01c085e61fd
miri04am <silviobettymiriam@gmail.com>
2020-03-23 11:36:01 +0100

    modificati file

--- main.py
+++ main.py
@@ -3,3 +3,8 @@ print("cioa", nome)
 anni = input("quanti anni hai")
 if anni >= 18:
 	print("sei maggiorenne")
+else:
+	print("sei ancora piccolo")
+viaggio = input("dove vuoi andare")
+if viaggio == "londra" and anni <= 18:
+	print("sorry cambia meta")
